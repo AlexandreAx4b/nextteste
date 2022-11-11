9a83c3e8d759f19962dc32f8fc5261510be1d7f5
Alexandre Carvalho <alexandre.carvalho@ax4b.com>
2022-11-11 16:30:46 -0300

    deploy aws

--- next.config.js
+++ next.config.js
@@ -1,12 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    images: {
-      unoptimized: true
-    }
+// next.config.js
+
+module.exports = {
+  compiler: {
+    // ssr and displayName are configured by default
+    styledComponents: true,
+  },
+  images: {
+    unoptimized: true
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/',
+        permanent: true,
+      },
+    ]
   },
 }
-
-module.exports = nextConfig
